a21bbbc09310093ade80db13564c88d89ca6e0e7
Marton Bur <marton.bur@gmail.com>
2015-02-05 18:20:33 +0100

    Added filter example project #11

---
+++ examples/hu.bme.mit.massif.examples.filter/src/hu/bme/mit/massif/examples/filter/MaskFilter.java
@@ -0,0 +1,41 @@
+package hu.bme.mit.massif.examples.filter;
+
+import hu.bme.mit.massif.communication.command.MatlabCommand;
+import hu.bme.mit.massif.communication.command.MatlabCommandFactory;
+import hu.bme.mit.massif.communication.datatype.MatlabString;
+import hu.bme.mit.massif.simulink.api.extension.ISimulinkImportFilter;
+
+public class MaskFilter implements ISimulinkImportFilter {
+
+	public MaskFilter() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String getName() {
+		return "MaskFilter";
+	}
+
+	@Override
+	public String getDescription() {
+		return "This is a user defined custom filter that ignores all masked subsystems.";
+	}
+
+	@Override
+	public boolean filter(MatlabCommandFactory commandFactory, String blockFQN) {
+		
+		String blockType = MatlabString.getMatlabStringData(commandFactory.getParam().addParam(blockFQN).addParam("blockType").execute());
+		if("SubSystem".equals(blockType)){		
+			// In case of a subsystem check mask
+			MatlabCommand getMask = commandFactory.getParam().addParam(blockFQN).addParam("Mask");
+			String isMaskOn = MatlabString.getMatlabStringData(getMask.execute());
+			
+			if("on".equals(isMaskOn)){
+				return true;
+			}
+		}
+		
+		return false;
+	}
+
+}

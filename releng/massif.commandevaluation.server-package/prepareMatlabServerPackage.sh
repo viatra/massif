9a9c5b4f8b7ac6df5e8eee54b793a20cdbd0e7fb
Ábel Hegedüs <abel.hegedus@gmail.com>
2015-05-05 13:51:32 +0200

    Update prepareMatlabServerPackage.sh
    fixed path to config file

--- releng/massif.commandevaluation.server-package/prepareMatlabServerPackage.sh
+++ releng/massif.commandevaluation.server-package/prepareMatlabServerPackage.sh
@@ -18,7 +18,7 @@ cp $PLUGINSDIR/br.com.embraer.massif.commandevaluation.base/target/br.com.embrae
 cp $PLUGINSDIR/br.com.embraer.massif.commandevaluation.server/target/br.com.embraer.massif.commandevaluation.server*-SNAPSHOT.jar $MATLABDIRPARENT/matlab/br.com.embraer.massif.commandevaluation.server.jar
 cp $PLUGINSDIR/br.com.embraer.massif.commandevaluation.server/setup_remote.m $MATLABDIRPARENT/matlab
 cp $PLUGINSDIR/hu.bme.mit.massif.simulink.api/scripts/library_collector.m $MATLABDIRPARENT/matlab
-cp $PLUGINSDIR/br.com.embraer.massif.commandevaluation.tests/testconfig/matlabserverconfig.properties $MATLABDIRPARENT/matlab
+cp $WORKSPACE/tests/br.com.embraer.massif.commandevaluation.tests/testconfig/matlabserverconfig.properties $MATLABDIRPARENT/matlab
 cd $MATLABDIRPARENT
 
 zip -r massif.commandevaluation.server.zip ./matlab

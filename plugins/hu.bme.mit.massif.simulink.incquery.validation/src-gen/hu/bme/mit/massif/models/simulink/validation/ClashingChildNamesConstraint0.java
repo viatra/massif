6b26edfe3bdf9ab8576fa11fcbdfe27713bf8140
Abel Hegedus <abel.hegedus@mit.bme.hu>
2015-07-06 12:57:08 +0200

    updated validation queries for new framework

--- plugins/hu.bme.mit.massif.simulink.incquery.validation/src-gen/hu/bme/mit/massif/models/simulink/validation/ClashingChildNamesConstraint0.java
+++ plugins/hu.bme.mit.massif.simulink.incquery.validation/src-gen/hu/bme/mit/massif/models/simulink/validation/ClashingChildNamesConstraint0.java
@@ -1,45 +1,81 @@
 package hu.bme.mit.massif.models.simulink.validation;
 
-import org.eclipse.emf.ecore.EObject;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
 
-import org.eclipse.incquery.validation.runtime.Constraint;
-import org.eclipse.incquery.validation.runtime.ValidationUtil;
-import org.eclipse.incquery.runtime.api.impl.BaseGeneratedEMFQuerySpecification;
+import org.eclipse.incquery.validation.core.api.Severity;
+import org.eclipse.incquery.validation.core.api.IConstraintSpecification;
+import org.eclipse.incquery.runtime.api.IPatternMatch;
+import org.eclipse.incquery.runtime.api.IQuerySpecification;
+import org.eclipse.incquery.runtime.api.IncQueryMatcher;
 import org.eclipse.incquery.runtime.exception.IncQueryException;
 
-import hu.bme.mit.massif.models.simulink.validation.ClashingChildNamesMatch;
 import hu.bme.mit.massif.models.simulink.validation.util.ClashingChildNamesQuerySpecification;
-import hu.bme.mit.massif.models.simulink.validation.ClashingChildNamesMatcher;
 
-public class ClashingChildNamesConstraint0 extends Constraint<ClashingChildNamesMatch> {
+public class ClashingChildNamesConstraint0 implements IConstraintSpecification {
 
-	private ClashingChildNamesQuerySpecification querySpecification;
+    private ClashingChildNamesQuerySpecification querySpecification;
 
-	public ClashingChildNamesConstraint0() throws IncQueryException {
-		querySpecification = ClashingChildNamesQuerySpecification.instance();
-	}
+    public ClashingChildNamesConstraint0() throws IncQueryException {
+        querySpecification = ClashingChildNamesQuerySpecification.instance();
+    }
 
-	@Override
-	public String getMessage() {
-		return "Child $child$ has a sibling in $parent$ with the same name!";
-	}
+    @Override
+    public String getMessageFormat() {
+        return "Child $child$ has a sibling in $parent$ with the same name!";
+    }
 
-	@Override
-	public EObject getLocationObject(ClashingChildNamesMatch signature) {
-		Object location = signature.get("child");
-		if(location instanceof EObject){
-			return (EObject) location;
-		}
-		return null;
-	}
 
-	@Override
-	public int getSeverity() {
-		return ValidationUtil.getSeverity("error");
-	}
+    @Override
+    public Map<String,Object> getKeyObjects(IPatternMatch signature) {
+        Map<String,Object> map = ImmutableMap.of(
+            "child",signature.get("child")
+        );
+        return map;
+    }
 
-	@Override
-	public BaseGeneratedEMFQuerySpecification<ClashingChildNamesMatcher> getQuerySpecification() {
-		return querySpecification;
-	}
+    @Override
+    public List<String> getKeyNames() {
+        List<String> keyNames = ImmutableList.of(
+            "child"
+        );
+        return keyNames;
+    }
+
+    @Override
+    public List<String> getPropertyNames() {
+        List<String> propertyNames = ImmutableList.of(
+            "parent"
+        );
+        return propertyNames;
+    }
+
+    @Override
+    public Set<List<String>> getSymmetricPropertyNames() {
+        Set<List<String>> symmetricPropertyNamesSet = ImmutableSet.<List<String>>of(
+        );
+        return symmetricPropertyNamesSet;
+    }
+
+    @Override
+    public Set<List<String>> getSymmetricKeyNames() {
+        Set<List<String>> symmetricKeyNamesSet = ImmutableSet.<List<String>>of(
+        );
+        return symmetricKeyNamesSet;
+    }
+
+    @Override
+    public Severity getSeverity() {
+        return Severity.ERROR;
+    }
+
+    @Override
+    public IQuerySpecification<? extends IncQueryMatcher<? extends IPatternMatch>> getQuerySpecification() {
+        return querySpecification;
+    }
+
 }
--- plugins/hu.bme.mit.massif.simulink.incquery.validation/src-gen/hu/bme/mit/massif/models/simulink/validation/IdentifierNameContainsSlashConstraint0.java
+++ plugins/hu.bme.mit.massif.simulink.incquery.validation/src-gen/hu/bme/mit/massif/models/simulink/validation/IdentifierNameContainsSlashConstraint0.java
@@ -1,45 +1,80 @@
 package hu.bme.mit.massif.models.simulink.validation;
 
-import org.eclipse.emf.ecore.EObject;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
 
-import org.eclipse.incquery.validation.runtime.Constraint;
-import org.eclipse.incquery.validation.runtime.ValidationUtil;
-import org.eclipse.incquery.runtime.api.impl.BaseGeneratedEMFQuerySpecification;
+import org.eclipse.incquery.validation.core.api.Severity;
+import org.eclipse.incquery.validation.core.api.IConstraintSpecification;
+import org.eclipse.incquery.runtime.api.IPatternMatch;
+import org.eclipse.incquery.runtime.api.IQuerySpecification;
+import org.eclipse.incquery.runtime.api.IncQueryMatcher;
 import org.eclipse.incquery.runtime.exception.IncQueryException;
 
-import hu.bme.mit.massif.models.simulink.validation.IdentifierNameContainsSlashMatch;
 import hu.bme.mit.massif.models.simulink.validation.util.IdentifierNameContainsSlashQuerySpecification;
-import hu.bme.mit.massif.models.simulink.validation.IdentifierNameContainsSlashMatcher;
 
-public class IdentifierNameContainsSlashConstraint0 extends Constraint<IdentifierNameContainsSlashMatch> {
+public class IdentifierNameContainsSlashConstraint0 implements IConstraintSpecification {
 
-	private IdentifierNameContainsSlashQuerySpecification querySpecification;
+    private IdentifierNameContainsSlashQuerySpecification querySpecification;
 
-	public IdentifierNameContainsSlashConstraint0() throws IncQueryException {
-		querySpecification = IdentifierNameContainsSlashQuerySpecification.instance();
-	}
+    public IdentifierNameContainsSlashConstraint0() throws IncQueryException {
+        querySpecification = IdentifierNameContainsSlashQuerySpecification.instance();
+    }
 
-	@Override
-	public String getMessage() {
-		return "Identifier $id$ must not contain \'/\'!";
-	}
+    @Override
+    public String getMessageFormat() {
+        return "Identifier $id$ must not contain \'/\'!";
+    }
 
-	@Override
-	public EObject getLocationObject(IdentifierNameContainsSlashMatch signature) {
-		Object location = signature.get("id");
-		if(location instanceof EObject){
-			return (EObject) location;
-		}
-		return null;
-	}
 
-	@Override
-	public int getSeverity() {
-		return ValidationUtil.getSeverity("error");
-	}
+    @Override
+    public Map<String,Object> getKeyObjects(IPatternMatch signature) {
+        Map<String,Object> map = ImmutableMap.of(
+            "id",signature.get("id")
+        );
+        return map;
+    }
 
-	@Override
-	public BaseGeneratedEMFQuerySpecification<IdentifierNameContainsSlashMatcher> getQuerySpecification() {
-		return querySpecification;
-	}
+    @Override
+    public List<String> getKeyNames() {
+        List<String> keyNames = ImmutableList.of(
+            "id"
+        );
+        return keyNames;
+    }
+
+    @Override
+    public List<String> getPropertyNames() {
+        List<String> propertyNames = ImmutableList.of(
+        );
+        return propertyNames;
+    }
+
+    @Override
+    public Set<List<String>> getSymmetricPropertyNames() {
+        Set<List<String>> symmetricPropertyNamesSet = ImmutableSet.<List<String>>of(
+        );
+        return symmetricPropertyNamesSet;
+    }
+
+    @Override
+    public Set<List<String>> getSymmetricKeyNames() {
+        Set<List<String>> symmetricKeyNamesSet = ImmutableSet.<List<String>>of(
+        );
+        return symmetricKeyNamesSet;
+    }
+
+    @Override
+    public Severity getSeverity() {
+        return Severity.ERROR;
+    }
+
+    @Override
+    public IQuerySpecification<? extends IncQueryMatcher<? extends IPatternMatch>> getQuerySpecification() {
+        return querySpecification;
+    }
+
 }

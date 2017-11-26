a51009b10f5a07592c7c09e933fff4f16e82bec6
lunkpeter <lunk.peter@incquerylabs.com>
2017-11-26 17:35:32 +0100

    Added CLI packaging script #63

---
+++ plugins/hu.bme.mit.massif.simulink.cli/scripts/CLI_import.m
@@ -0,0 +1,19 @@
+%##############################################################################
+% Copyright (c) 2010-2013, Embraer S.A., Budapest University of Technology and Economics
+% All rights reserved. This program and the accompanying materials 
+% are made available under the terms of the Eclipse Public License v1.0 
+% which accompanies this distribution, and is available at 
+% http://www.eclipse.org/legal/epl-v10.html 
+%
+% Contributors: 
+%     Rodrigo Rizzi Starr, Lincoln Nascimento - initial API and implementation 
+%##############################################################################
+javaaddpath('br.com.embraer.massif.commandevaluation.base.jar');
+javaaddpath('br.com.embraer.massif.commandevaluation.commands.jar');
+javaaddpath('br.com.embraer.massif.commandevaluation.server.jar');
+
+pid = feature('getpid');
+ver = strcat('r',version('-release'))
+
+modelProviderServer = br.com.embraer.massif.commandevaluation.server.ServerEntryPoint;
+modelProviderServer.startRMIServer(ver,num2str(pid),'')
---
+++ releng/hu.bme.mit.massif.simulink.cli-package/prepareCLIPackage.sh
@@ -0,0 +1,25 @@
+###############################################################################
+# Copyright (c) 2010-2017, IncQueryLabs Ltd., Embraer S.A., Budapest University of Technology and Economics
+# All rights reserved. This program and the accompanying materials 
+# are made available under the terms of the Eclipse Public License v1.0 
+# which accompanies this distribution, and is available at 
+# http://www.eclipse.org/legal/epl-v10.html 
+#
+# Contributors: 
+#     Peter Lunk - initial API and implementation 
+###############################################################################
+TARGETDIR=$WORKSPACE/releng/hu.bme.mit.massif.simulink.cli-package
+MATLABDIRPARENT=$TARGETDIR
+PLUGINSDIR=$WORKSPACE/plugins
+
+mkdir -p $MATLABDIRPARENT/matlab
+
+cp $TARGETDIR/target/hu.bme.mit.massif.simulink.cli-package*-SNAPSHOT.jar $TARGETDIR/matlab/hu.bme.mit.massif.simulink.cli-package.jar
+cp $PLUGINSDIR/hu.bme.mit.massif.simulink.api/scripts/library_collector.m $MATLABDIRPARENT/matlab
+
+cp $PLUGINSDIR/hu.bme.mit.massif.simulink.cli/scripts/CLI_export.m $MATLABDIRPARENT/matlab
+cp $PLUGINSDIR/hu.bme.mit.massif.simulink.cli/scripts/CLI_import.m $MATLABDIRPARENT/matlab
+
+cd $MATLABDIRPARENT
+
+zip -r hu.bme.mit.massif.simulink.cli-package.zip ./matlab

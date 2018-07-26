3ab7a10e8595f65989915dd2b81f7ddb3f52c3e9
Marton Bur <marton.bur@gmail.com>
2018-07-26 13:15:48 +0200

    Adding initial script-based block parameter query #102

---
+++ plugins/hu.bme.mit.massif.simulink.api/scripts/get_all_block_parameters.m
@@ -0,0 +1,26 @@
+%##############################################################################
+% Copyright (c) 2010-2018, IncQuery Labs Ltd., logi.cals GmbH, McGill University
+% All rights reserved. This program and the accompanying materials 
+% are made available under the terms of the Eclipse Public License v1.0 
+% which accompanies this distribution, and is available at 
+% http://www.eclipse.org/legal/epl-v10.html 
+%
+% Contributors: 
+%     Marton Bur - initial API and implementation 
+%##############################################################################
+function s = get_all_block_parameters(blockId)
+%get_all_block_parameters Gathers all block parameters to a struct
+%   The following block parameters are skipped because of adapter limitations:
+%    * Capabilities
+%    * MaskObject
+s=struct();
+TmpObjParams=get_param(blockId,'ObjectParameters');
+names=fieldnames(TmpObjParams);
+for i = 1:numel(names)
+    if strcmpi(names{i},'Capabilities') == 0 || strcmpi(names{i},'MaskObject') == 0
+        TmpParamValue=get_param(blockId,names{i});
+        if strcmp(class(TmpParamValue), "char") == 0 
+            [s(:).(names{i})]=TmpParamValue;
+        end
+    end
+end
--- releng/massif.commandevaluation.server-package/prepareMatlabServerPackage.sh
+++ releng/massif.commandevaluation.server-package/prepareMatlabServerPackage.sh
@@ -19,6 +19,7 @@ cp $PLUGINSDIR/br.com.embraer.massif.commandevaluation.commands/target/br.com.em
 cp $PLUGINSDIR/br.com.embraer.massif.commandevaluation.server/target/br.com.embraer.massif.commandevaluation.server*-SNAPSHOT.jar $MATLABDIRPARENT/matlab/br.com.embraer.massif.commandevaluation.server.jar
 cp $PLUGINSDIR/br.com.embraer.massif.commandevaluation.server/setup_remote.m $MATLABDIRPARENT/matlab
 cp $PLUGINSDIR/hu.bme.mit.massif.simulink.api/scripts/library_collector.m $MATLABDIRPARENT/matlab
+cp $PLUGINSDIR/hu.bme.mit.massif.simulink.api/scripts/get_all_block_parameters.m $MATLABDIRPARENT/matlab
 cp $WORKSPACE/tests/br.com.embraer.massif.commandevaluation.tests/testconfig/matlabserverconfig.properties $MATLABDIRPARENT/matlab
 cd $MATLABDIRPARENT
 

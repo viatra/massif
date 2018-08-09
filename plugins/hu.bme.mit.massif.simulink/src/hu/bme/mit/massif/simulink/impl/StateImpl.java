c31c9ab530223207e919e628f9124863f24aa01a
Marton Bur <marton.bur@gmail.com>
2018-08-09 15:39:21 +0200

    Adding missin (c) headers #8

--- plugins/hu.bme.mit.massif.simulink/src/hu/bme/mit/massif/simulink/impl/StateImpl.java
+++ plugins/hu.bme.mit.massif.simulink/src/hu/bme/mit/massif/simulink/impl/StateImpl.java
@@ -1,5 +1,13 @@
-/**
- */
+/*******************************************************************************
+ * Copyright (c) 2010-2018, IncQuery Labs Ltd., logi.cals GmbH, McGill University
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Eclipse Public License v1.0 
+ * which accompanies this distribution, and is available at 
+ * http://www.eclipse.org/legal/epl-v10.html 
+ *
+ * Contributors: 
+ *     Marton Bur - initial API and implementation 
+ *******************************************************************************/
 package hu.bme.mit.massif.simulink.impl;
 
 import hu.bme.mit.massif.simulink.SimulinkPackage;

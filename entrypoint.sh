c9c168fd12c10a6dd585ecb08f097321f5d496b3
tamasSzentandrasi <tamas.szentandrasi@incquerylabs.com>
2020-07-27 13:24:58 +0200

    noclone fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ vncserver :1 -geometry 1280x800 -depth 24
 vnc_pid=$!
 export DISPLAY=:1
 
-mvn clean install -B -f ${GITHUB_WORKSPACE}/releng/hu.bme.mit.massif.parent/pom.xml -Dmaven.repo.local=.repository
+mvn clean install -B -e -X -f ${GITHUB_WORKSPACE}/releng/hu.bme.mit.massif.parent/pom.xml -Dmaven.repo.local=.repository
